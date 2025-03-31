93e6c34351fc33e04051f65df3a853ac9e7eb7d5
Beatriz <beatriz.giovanna@gmail.com>
2025-03-30 23:11:08 -0300

    finaliza programa

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+
+Console.WriteLine("--------- Forca ---------");
+Console.WriteLine("Digite  a palavra secreta");
+string palavra = Console.ReadLine()!.ToLower();
+
+Console.WriteLine("Digite uma letra");
+string letra = Console.ReadLine()!.ToLower();
+
+bool existe = palavra.Contains(letra);
+Console.WriteLine($"A letra \"{letra.ToUpper()}\" existe na palavra secreta => {existe}");
